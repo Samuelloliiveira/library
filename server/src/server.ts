419ee3274521c33ae4c755ac732f1793f7cdb0f6
Samuel Oliveira <samuelcontatodev@gmail.com>
2023-03-13 15:18:50 -0300

    rota de criação de livros

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,33 @@
+import dayjs from 'dayjs'
 import Fastify from 'fastify'
+import { z } from 'zod'
+import { PrismaClient } from '@prisma/client'
 
 const app = Fastify()
 
-app.get('/', async (request, reply) => {
-  return { hello: 'world' }
+const prisma = new PrismaClient()
+
+app.post('/books', async (request, reply) => {
+  const createBookBody = z.object({
+    title: z.string(),
+    author: z.string(),
+    bookReleaseDate: z.string(),
+  })
+
+  const { title, author, bookReleaseDate } = createBookBody.parse(request.body)
+
+  const today = dayjs().startOf('day').toDate()
+
+  await prisma.book.create({
+    data: {
+      title,
+      author,
+      book_release_date: bookReleaseDate,
+      crated_at: today
+    }
+  })
+
+  return reply.status(201).send()
 })
 
 app.listen({
